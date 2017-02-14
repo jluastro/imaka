10a3b17d4d5c81824c7f2dee667365b1d2d245b1
Jessica Lu <jlu.astro@yahoo.com>
2017-02-13 21:47:49 -0800

    Added routine to fetch stats FITS tables from onaga. This is just handy.

--- imaka/analysis/plot_stats.py
+++ imaka/analysis/plot_stats.py
@@ -1,1 +1,45 @@
+from astropy.table import Table
+import subprocess
+import os
+from imaka.reduce import util
 
+
+def fetch_stats_from_onaga(dates, output_root):
+    """
+    SCP stats files (FITS tables) from onaga and place them in a mirrored 
+    directory structure on your local machine. 
+
+    Parameters
+    ----------
+    dates : list/array of strings
+        List of date strings (e.g. '20170113') to process.
+        You can also use 'all' or None and the entire available 
+        list of all dates will be used. 
+    output_root : str
+        The root directory where the transferred files will be stored. Note
+        that this is just the root and under this will be stored a structure
+        that parallels that on onaga:
+            <output_root>/<date>/fli/reduce/stats/*
+    """
+    all_dates = ['20170110', '20170111', '20170112', '20170113']
+
+    if dates == 'all' or dates == None:
+        dates = all_dates
+
+    for date in dates:
+        remote_file = 'imaka@onaga.ifa.hawaii.edu:/Volumes/DATA/imaka/{0:s}/fli/reduce/stats/stats*.fits'.format(date)
+        destination = '{0:s}/{1:s}/fli/reduce/stats/'.format(output_root, date)
+
+        util.mkdir(destination)
+
+        print(remote_file)
+        print(destination)
+        p = subprocess.Popen(["scp", remote_file, destination])
+        sts = os.waitpid(p.pid, 0)
+
+    return
+    
+
+def plot_stack_stats():
+    
+    return
