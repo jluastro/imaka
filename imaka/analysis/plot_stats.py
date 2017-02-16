49e8f2f6b0c680e05a56aa374a8cf6adfa9b6039
Jessica Lu <jlu.astro@yahoo.com>
2017-02-15 22:22:26 -0800

    Added load stats tables for onaga.

--- imaka/analysis/plot_stats.py
+++ imaka/analysis/plot_stats.py
@@ -32,7 +32,7 @@ def fetch_stats_from_onaga(dates, output_root):
         dates = all_dates
 
     for date in dates:
-        remote_file = 'imaka@onaga.ifa.hawaii.edu:/Volumes/DATA/imaka/{0:s}/fli/reduce/stats/stats*.fits'.format(date)
+        remote_file = 'imaka@onaga.ifa.hawaii.edu:/Volumes/DATA4/imaka/{0:s}/fli/reduce/stats/stats*.fits'.format(date)
         destination = '{0:s}/{1:s}/fli/reduce/stats/'.format(output_root, date)
 
         util.mkdir(destination)
@@ -43,7 +43,40 @@ def fetch_stats_from_onaga(dates, output_root):
         sts = os.waitpid(p.pid, 0)
 
     return
-    
+
+def load_stats_from_onaga(dates, output_root):
+    """
+    SCP stats files (FITS tables) from onaga and place them in a mirrored 
+    directory structure on your local machine. 
+
+    Parameters
+    ----------
+    dates : list/array of strings
+        List of date strings (e.g. '20170113') to process.
+        You can also use 'all' or None and the entire available 
+        list of all dates will be used. 
+    output_root : str
+        The root directory where the transferred files will be stored. Note
+        that this is just the root and under this will be stored a structure
+        that parallels that on onaga:
+            <output_root>/<date>/fli/reduce/stats/*
+    """
+    all_dates = ['20170110', '20170111', '20170112', '20170113']
+
+    if dates == 'all' or dates == None:
+        dates = all_dates
+
+    for date in dates:
+        local_file = '{0:s}/{1:s}/fli/reduce/stats/stats*.fits'.format(output_root, date)
+        destination = 'imaka@onaga.ifa.hawaii.edu:/Volumes/DATA4/imaka/{0:s}/fli/reduce/stats/'.format(date)
+
+        print(local_file)
+        print(destination)
+        p = subprocess.Popen(["scp", local_file, destination])
+        sts = os.waitpid(p.pid, 0)
+
+    return
+
 
 def plot_stack_stats(date, suffix='', root_dir='/Users/jlu/work/imaka/pleiades/'):
     """
