49af5c8851d0f8ed94a270b624d5b2561b350ab8
Jessica Lu <jlu.astro@yahoo.com>
2017-02-16 00:34:40 -0800

    Adding in revised MASS/DIMM scripts and example in 2017_01_13 notebook.

--- imaka/reduce/massdimm.py
+++ imaka/reduce/massdimm.py
@@ -12,11 +12,13 @@
 '''
 
 import numpy as np
-import os
+import os, urllib
 from astropy.io import fits
-from astropy.table import Table
+from astropy.table import Table, Column
 from pandas import read_csv
-from datetime import datetime
+import datetime
+import pytz
+from imaka.reduce import util
 import socket
 import pdb
 
@@ -35,103 +37,6 @@ def plotRo(dateSuffix):
     py.clf()
     py.plot(dimm.r0)
 
-class Image(object):
-    ''' Input a FITS image '''
-    def __init__ (self, imgfile):
-        self.infile = imgfile
-
-        self.hdulist = fits.open(self.infile)
-
-    def time(self):
-        ''' Extracts time from FITS header'''
-
-        datetime = self.hdulist[0].header['DATETIME']
-
-        self.hour   = float(datetime[-6:-4])
-        self.minute = float(datetime[-4:-2])
-        self.second = float(datetime[-2:])
-
-        # Convert from HST to UT
-        self.hour += 10
-        
-        if self.hour >= 24:
-            self.hour -= 24
-
-        self.timeInHours = self.hour + (self.minute/60.0) + (self.second/3600.0)
-
-        return np.array([self.timeInHours])
-
-
-class StatsTable(object):
-    ''' Input a FITS table '''
-    def __init__ (self, tablefile):
-        self.infile = tablefile
-
-        self.data = Table.read(tablefile)
-
-    def time(self):
-        ''' Extracts time from FITS table. Returns array of hh, mm, ss '''
-        self.times   = self.data['TIME_UTC']
-
-        self.times   = np.array([item for item in self.times.split(':')]).astype('float')
-        self.time_24 = self.times[:,0] + self.times[:,1]/60. + self.times[:,2]/3600.
-
-        return self.time_24
-
-    def get_datetimes(self):
-        dt_utc = []
-        for ii in range(len(self.data)):
-            dt_str = self.data['DATE_UTC'] + ' ' + self.data['TIME_UTC'][ii]
-            dt.append( datetime.strptime(dt_str, '%Y:%m:%d %H:%M:%S') )
-
-        return dt_utc
-    
-    def fwhm(self):
-        self.fwhm  = self.data['FWHM']
-        return self.fwhm
-
-    def EE80(self):
-        self.ee80  = self.data['EE80']
-        return self.ee80
-    
-    def add_mass(self, masscolumn):
-        ''' Adds new MASS column to table '''
-        orig_cols  = self.data.columns
-        new_cols   = fits.ColDefs(\
-            [fits.Column(name='MASS', format='D', array=masscolumn)])
-        self.data = fits.BinTableHDU.from_columns(orig_cols + new_cols)
-
-        return
-
-    def add_dimm(self, dimmcolumn):
-        ''' Adds new DIMM column to table '''
-        orig_cols  = self.data.columns
-        new_cols   = fits.ColDefs(\
-            [fits.Column(name='DIMM', format='D', array=dimmcolumn)])
-        self.data = fits.BinTableHDU.from_columns(orig_cols + new_cols)
-
-        return
-    
-    def add_profile(self, profcols):
-        ''' Adds new profile columns to table '''
-        orig_cols  = self.data.columns
-
-        new_cols   = fits.ColDefs(\
-            [fits.Column(name='Cn2dh_05', format='D', array=profcols[0]), \
-             fits.Column(name='Cn2dh_1' , format='D', array=profcols[1]), \
-             fits.Column(name='Cn2dh_2' , format='D', array=profcols[2]), \
-             fits.Column(name='Cn2dh_4' , format='D', array=profcols[3]), \
-             fits.Column(name='Cn2dh_8' , format='D', array=profcols[4]), \
-             fits.Column(name='Cn2dh_16', format='D', array=profcols[5])  ])
-        self.data = fits.BinTableHDU.from_columns(orig_cols + new_cols)
-
-        return
-    
-
-    def output(self):
-        ''' Write modified table '''
-        self.data.writeto(self.infile[:-5] + '_mdp' + '.fits')
-        return
 
 class DIMM(object):
     def __init__(self, dimmfile):
@@ -178,7 +83,7 @@ class DIMM(object):
                 closestVal = self.seeing[closestIndex]
             closestVals.append(closestVal)
 
-        return closestVals
+        return closestVals, timeDiff
 
 class MASS(object):
 
@@ -226,7 +131,7 @@ class MASS(object):
                 closestVal = self.free_seeing[closestIndex]
             closestVals.append(closestVal)
 
-        return closestVals
+        return closestVals, timeDiff
 
 class MASSPROF(object):
 
@@ -278,7 +183,7 @@ class MASSPROF(object):
 
         closestVals = np.array(closestVals).T
 
-        return closestVals
+        return closestVals, timeDiff
 
 def fetch_data(utDate, saveTo):
     ''' Saves massdimm files to directory specified.'''
@@ -287,22 +192,24 @@ def fetch_data(utDate, saveTo):
     print('Saving MASS/DIMM data to directory:')
     print(saveTo)
 
+    util.mkdir(saveTo)
+
     urlRoot = 'http://mkwc.ifa.hawaii.edu/current/seeing/'
     
     # Save the MASS file
     massFile = utDate + '.mass.dat'
     url = urlRoot + 'mass/' + massFile
-    urllib.urlretrieve(url, saveTo + massFile)
+    urllib.request.urlretrieve(url, saveTo + massFile)
 
     # Save the DIMM file
     dimmFile = utDate + '.dimm.dat'
     url = urlRoot + 'dimm/' + dimmFile
-    urllib.urlretrieve(url, saveTo + dimmFile)
+    urllib.request.urlretrieve(url, saveTo + dimmFile)
 
     # Save the MASS profile
     massproFile = utDate + '.masspro.dat'
     url = urlRoot + 'masspro/' + massproFile
-    urllib.urlretrieve(url, saveTo + massproFile)
+    urllib.request.urlretrieve(url, saveTo + massproFile)
     
 
 def append_mass_dimm(stats_file, massdimm_dir, realtime=False):
@@ -322,112 +229,121 @@ def append_mass_dimm(stats_file, massdimm_dir, realtime=False):
 
     # Read in the stats file, which contains the times of the
     # individual exposures. 
-    stats = StatsTable(stats_file)
+    stats = Table.read(stats_file)
 
-    # Fetch the UT dates and times from the table. 
-    dt_utc = datafile.get_datetimes()
+    # Fetch the UT dates and times from the table.
 
-    # Get the set of unique UT Dates. 
+    # Loop through match up with the mass/dimm values.
+    date_long_previous = None
 
-
+    # Some output tables.
+    all_dimm = np.zeros(len(stats), dtype=float)
+    all_mass = np.zeros(len(stats), dtype=float)
+    all_prof_005 = np.zeros(len(stats), dtype=float)
+    all_prof_010 = np.zeros(len(stats), dtype=float)
+    all_prof_020 = np.zeros(len(stats), dtype=float)
+    all_prof_040 = np.zeros(len(stats), dtype=float)
+    all_prof_080 = np.zeros(len(stats), dtype=float)
+    all_prof_160 = np.zeros(len(stats), dtype=float)
+    all_dimm_dt = np.zeros(len(stats), dtype=float)
+    all_mass_dt = np.zeros(len(stats), dtype=float)
+    all_prof_dt = np.zeros(len(stats), dtype=float)
     
+    for ii in range(len(stats)):
+        dt_str = stats['DATE_UTC'][ii] + ' ' + stats['TIME_UTC'][ii]
+        dt_utc = datetime.datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
 
-    dimmvalue  = DIMM(massdimmdir    +td+'.dimm.dat')   .indexTime(datatime)
-    massvalue  = MASS(massdimmdir    +td+'.mass.dat')   .indexTime(datatime)
-    profvalues = MASSPROF(massdimmdir+td+'.masspro.dat').indexTime(datatime)
+        date_long = '{0:4d}{1:02d}{2:02d}'.format(dt_utc.year,
+                                                  dt_utc.month,
+                                                  dt_utc.day)
+        time_hour = dt_utc.hour + dt_utc.minute / 60.0 + dt_utc.second / 3600.
 
-    ## display results
-    if isfitsimg:
-    	print('MASS:', massvalue, 'DIMM:', dimmvalue)
-    else:
-        if filename.endswith('md.fits'):
-            pass
-        else:
-            datafile.add_dimm(dimmvalue)
-            datafile.add_mass(massvalue)
-            datafile.add_profile(profvalues)
-            datafile.output()
-            #output(td, datafile.r0(), datafile.EE80(), massvalue, dimmvalue)
+        # Reload the MASS/DIMM tables if we update the day.
+        if date_long != date_long_previous:
+            t_dimm = DIMM(massdimm_dir + date_long + '.dimm.dat')
+            t_mass = MASS(massdimm_dir + date_long + '.mass.dat')
+            t_prof = MASSPROF(massdimm_dir + date_long + '.masspro.dat')
 
-    print('Done!')
-
-
-    
-    
-    
-if __name__ == "__main__":
-    ## initialize ##
-    realtime = False    #set by hand
-    
-    ## When running on onaga
-    if socket.gethostname() == 'onaga.ifa.hawaii.edu':
+        dimm_val, dimm_dt = t_dimm.indexTime([time_hour])
+        mass_val, mass_dt = t_mass.indexTime([time_hour])
+        prof_val, prof_dt = t_prof.indexTime([time_hour])
         
-        if realtime == True:
-            ## get most recent mass/dimm files. 
-            ## only need to do this when processing data in real time
-            date  = datetime.utcnow().timetuple()
-            td    = '%i%02d%02d' %(date.tm_year, date.tm_mon, date.tm_mday)
+        all_dimm[ii] = dimm_val[0]
+        all_mass[ii] = mass_val[0]
 
-            massdimmdir = '/Volumes/DATA/imaka/' + td + '/mkwc/'
+        all_prof_005[ii] = prof_val[0][0]
+        all_prof_010[ii] = prof_val[1][0]
+        all_prof_020[ii] = prof_val[2][0]
+        all_prof_040[ii] = prof_val[3][0]
+        all_prof_080[ii] = prof_val[4][0]
+        all_prof_160[ii] = prof_val[5][0]
 
-            try:
-                fetch_data(td, massdimmdir)
-            except:
-                'Most recent MASS/DIMM data not updated.\n'
+        all_dimm_dt[ii] = dimm_dt[0]
+        all_mass_dt[ii] = mass_dt[0]
+        all_prof_dt[ii] = prof_dt[0]
 
-        #datadir     = '/Volumes/DATA/imaka/20170111/ao/'
-        #filename    = 'aocb_20170111_205.fits'
-        td          = '20170114'  # <-- set this by hand
-        filename    = 'stats_closed2.fits'  # <-- set this by hand
-        ## The format bit is needed when the data directory is in HST on 201701 and before
-        datadir     = '/Volumes/DATA4/imaka/{0}/fli/reduce/stats/'.format(int(float(td))-1)
-        massdimmdir = '/Volumes/DATA4/imaka/{0}/mkwc/'.format(int(float(td))-1)
+        
+    col_dimm = Column(name='DIMM', data=all_dimm)
+    col_mass = Column(name='MASS', data=all_mass)
+    col_prof_005 = Column(name='Cn2dh_005', data=all_prof_005)
+    col_prof_010 = Column(name='Cn2dh_010', data=all_prof_010)
+    col_prof_020 = Column(name='Cn2dh_020', data=all_prof_020)
+    col_prof_040 = Column(name='Cn2dh_040', data=all_prof_040)
+    col_prof_080 = Column(name='Cn2dh_080', data=all_prof_080)
+    col_prof_160 = Column(name='Cn2dh_160', data=all_prof_160)
+    col_dimm_dt = Column(name='DIMM_dt_hr', data=all_dimm_dt)
+    col_mass_dt = Column(name='MASS_dt_hr', data=all_mass_dt)
+    col_prof_dt = Column(name='Cn2dh_dt_hr', data=all_prof_dt)
+    
+    
+    stats.add_columns([col_dimm, col_dimm_dt, col_mass, col_mass_dt,
+                       col_prof_005, col_prof_010, col_prof_020,
+                       col_prof_040, col_prof_080, col_prof_160, col_prof_dt])
 
+    stats_file_root, stats_file_ext = os.path.splitext(stats_file)
+    stats.write(stats_file_root + '_mdp' + stats_file_ext, overwrite=True)
+    
+    return
+
+
+def get_mass_dimm_for_image(fits_file, massdimm_dir, realtime=False):
+    hdr = fits.getheader(fits_file)
+    
+    time_tmp = hdr['TIMEOBS']
+    date_tmp = hdr['DATEOBS']
+    hst_tz = pytz.timezone('US/Hawaii')
+    
+    if hdr['SHUTTER'] == True:
+        dt_hst = datetime.datetime.strptime(date_tmp + ' ' + time_tmp, '%m/%d/%Y %H:%M:%S')
+        dt_hst = hst_tz.localize(dt_hst)
     else:
-        ## When running on local files
+        dt_hst = datetime.datetime.strptime(date_tmp + ' ' + time_tmp, '%d/%m/%Y %I:%M:%S %p')
+        dt_hst = hst_tz.localize(dt_hst)
 
-        #datadir     = '/Users/dorafohring/Desktop/imaka/data/20170111/'
-        #filename    = 'aocb_20170111_205.fits'
+        # make hst date switch over at midnight 
+        noon = datetime.time(12, 0, 0) # assuming you'll never be taking images at local noon...
+        del_day = datetime.timedelta(days=1)
+        if dt_hst.time() < noon:
+            dt_hst += del_day
+        
+    dt_utc = dt_hst.astimezone(pytz.utc)
 
-        td          = '20170112'  # <-- set this by hand
-        filename    = 'stats_closed1.fits'
-        datadir     = '/Users/dorafohring/Desktop/imaka/data/{0}/'.format(int(float(td))-1)
-        massdimmdir = '/Users/dorafohring/Desktop/imaka/massdimm/'
+    date_long = '{0:4d}{1:02d}{2:02d}'.format(dt_utc.year,
+                                              dt_utc.month,
+                                              dt_utc.day)
+    time_hour = dt_utc.hour + dt_utc.minute / 60.0 + dt_utc.second / 3600.
 
-    file = datadir + filename
+    # Reload the MASS/DIMM tables if we update the day.
+    t_dimm = DIMM(massdimm_dir + date_long + '.dimm.dat')
+    t_mass = MASS(massdimm_dir + date_long + '.mass.dat')
+                            
+    val_dimm = t_dimm.indexTime([time_hour])[0]
+    val_mass = t_mass.indexTime([time_hour])[0]
 
-    ## optional: can give filename as argument
-    if len(os.sys.argv) == 2:
-    	file = os.sys.argv[1]
-
-    ## determine if data is image or table based on name
-    if filename.startswith('stats'):
-    	isfitsimg = False
-    	datafile  = Table(file)	
-    else:
-    	isfitsimg = True
-    	datafile  = Image(file)
-
-    ## get mass/dimm times
-    datatime = datafile.time()
-
-    dimmvalue  = DIMM(massdimmdir    +td+'.dimm.dat')   .indexTime(datatime)
-    massvalue  = MASS(massdimmdir    +td+'.mass.dat')   .indexTime(datatime)
-    profvalues = MASSPROF(massdimmdir+td+'.masspro.dat').indexTime(datatime)
-
-    ## display results
-    if isfitsimg:
-    	print('MASS:', massvalue, 'DIMM:', dimmvalue)
-    else:
-        if filename.endswith('md.fits'):
-            pass
-        else:
-            datafile.add_dimm(dimmvalue)
-            datafile.add_mass(massvalue)
-            datafile.add_profile(profvalues)
-            datafile.output()
-            #output(td, datafile.r0(), datafile.EE80(), massvalue, dimmvalue)
-
-    print('Done!')
+    print('MASS:', val_mass, 'DIMM:', val_dimm)
+    
+    return
+    
+    
 
 
