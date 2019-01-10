34d5c065ad85af586bbe6a35a710ba4318c6217c
imaka <imaka@inaga.ifa.hawaii.edu>
2019-01-09 19:51:47 -1000

    merge

--- reduce/nights/reduce_2018_12_23.py
+++ reduce/nights/reduce_2018_12_23.py
@@ -105,31 +105,31 @@ def find_stars_orion():
 
     # Open Loop
     img_files = [out_dir + 'obj{0:03d}_o_scan_clean.fits'.format(ii) for ii in fnum_o]
-    reduce_fli.find_stars(img_files, fwhm=8, threshold=5, N_passes=2, plot_psf_compare=False, \
+    reduce_fli.find_stars(img_files, fwhm=8, threshold=3, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
     
     #Closed Loop - 4W
-    img_files = [out_dir + 'obj{0:03}dLS4WFS_scan_clean.fits'.format(ii) for ii in fnum_c_4W]
-    reduce_fli.find_stars(img_files, fwhm=7, threshold=5, N_passes=2, plot_psf_compare=False, \
+    img_files = [out_dir + 'obj{0:03d}LS4WFS_c_scan_clean.fits'.format(ii) for ii in fnum_c_4W]
+    reduce_fli.find_stars(img_files, fwhm=7, threshold=3, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
 
     #Closed Loop - B2
     img_files = [out_dir + 'obj{0:03d}LS4WFS_B2_c_scan_clean.fits'.format(ii) for ii in fnum_c_B2]
-    reduce_fli.find_stars(img_files, fwhm=7, threshold=5, N_passes=2, plot_psf_compare=False, \
+    reduce_fli.find_stars(img_files, fwhm=7, threshold=3, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
 
     #Closed Loop - zc
     img_files = [out_dir + 'obj{0:03d}LS4WFS_zc11_c_scan_clean.fits'.format(ii) for ii in fnum_c_zc]
-    reduce_fli.find_stars(img_files, fwhm=7, threshold=5, N_passes=2, plot_psf_compare=False, \
+    reduce_fli.find_stars(img_files, fwhm=7, threshold=3, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
 
     #Tip tilt
-    img_files = [out_dir + 'obj{0:03d}tip_tilt_scan.fits'.format(ii) for ii in fnum_c_tt]
-    reduce_fli.find_stars(img_files, fwhm=7, threshold=10, N_passes=2, plot_psf_compare=False, \
+    img_files = [out_dir + 'obj{0:03d}tip_tilt_scan_clean.fits'.format(ii) for ii in fnum_c_tt]
+    reduce_fli.find_stars(img_files, fwhm=7, threshold=3, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
                           
