3470d0327ef0b0cf36668f12382e75597b415858
Fatima Abdurrahman <fatima@orca.astro.berkeley.edu>
2019-02-19 12:13:20 -0800

    reduce

--- reduce/nights/reduce_2018_12_22.py
+++ reduce/nights/reduce_2018_12_22.py
@@ -27,17 +27,13 @@ stacks_dir = root_dir + 'reduce/stacks/'
 twi_dir = root_dir + 'twilights/'
 massdimm_dir = root_dir + 'reduce/massdimm/'
 
-fnum_o = [215, 219, 223, 227, 231, 235, 239, 243, 247, 251, 255, 259, 263, 267, 271, 276]
-fnum_o = [64, 68, 72, 76, 82, 87, 91, 98, 102, 106, 110, 114, 118, 122, 126, 130, 135, 139, 143, 147, 151, 155, 159, 163, 167, 171, 175, 179, 183, 187, 191, 195, 199, 203, 207, 211]
+fnum_o = [64, 68, 72, 76, 82, 87, 91, 98, 102, 106, 110, 114, 118, 122, 126, 130, 135, 139, 143, 147, 151, 155, 159, 163, 167, 171, 175, 179, 183, 187, 191, 195, 199, 203, 207, 211, 215, 219, 223, 227, 231, 235, 239, 243, 247, 251, 255, 259, 263, 267, 271, 276]
 
-fnum_c_4W = [212, 216, 220, 224, 228, 232, 236, 240, 244, 248, 252, 256, 260, 264, 268, 273]
-fnum_c_4W = [63, 65, 69, 73, 77, 79, 83, 88, 92, 95, 99, 103, 107, 111, 115, 119, 123, 127, 131, 132, 136, 140, 144, 148, 152, 156, 160, 164, 168, 172, 176, 180, 184, 188, 192, 196, 200, 204, 208]
+fnum_c_4W = [63, 65, 69, 73, 77, 79, 83, 88, 92, 95, 99, 103, 107, 111, 115, 119, 123, 127, 131, 132, 136, 140, 144, 148, 152, 156, 160, 164, 168, 172, 176, 180, 184, 188, 192, 196, 200, 204, 208, 212, 216, 220, 224, 228, 232, 236, 240, 244, 248, 252, 256, 260, 264, 268, 273]
 
-fnum_c_B2 = [213, 217, 221, 225, 229, 233, 237, 241, 245, 249, 254, 257, 261, 265, 269, 274]
-fnum_c_B2 = [66, 70, 74, 80, 85, 89, 93, 96, 100, 104, 108, 112, 116, 120, 124, 128, 133, 137, 141, 145, 149, 153, 157, 161, 165, 169, 173, 177, 181, 185, 189, 193, 197, 201, 205, 209]
+fnum_c_B2 = [66, 70, 74, 80, 85, 89, 93, 96, 100, 104, 108, 112, 116, 120, 124, 128, 133, 137, 141, 145, 149, 153, 157, 161, 165, 169, 173, 177, 181, 185, 189, 193, 197, 201, 205, 209, 213, 217, 221, 225, 229, 233, 237, 241, 245, 249, 253, 257, 261, 265, 269, 274]
 
-fnum_c_zc = [214, 218, 222, 226, 230, 234, 238, 242, 246, 250, 254, 258, 262, 266, 279, 275]
-fnum_c_zc = [67, 71, 75,  81, 86, 90, 97, 101, 105, 109, 113, 117, 121, 125, 129, 134, 138, 142, 146, 150, 154, 158, 162, 166, 170, 174, 178, 182, 186, 190, 194, 198, 202, 206, 210]
+fnum_c_zc = [67, 71, 75,  81, 86, 90, 97, 101, 105, 109, 113, 117, 121, 125, 129, 134, 138, 142, 146, 150, 154, 158, 162, 166, 170, 174, 178, 182, 186, 190, 194, 198, 202, 206, 210, 214, 218, 222, 226, 230, 234, 238, 242, 246, 250, 254, 258, 262, 266, 270, 275]
 
 
 def make_flat(): 
@@ -72,7 +68,7 @@ def reduce_orion():
 
     # Open Loop
     img_files = [data_dir + 'obj{0:03d}_o.fits'.format(ii) for ii in fnum_o]
-    #reduce_STA.treat_overscan(img_files)
+    reduce_STA.treat_overscan(img_files)
     scan_img_files = [data_dir + 'obj{0:03d}_o_scan.fits'.format(ii) for ii in fnum_o]
     reduce_fli.clean_images(scan_img_files, out_dir, rebin=1, sky_frame=sky_dir + 'orion_sky.fits', flat_frame=flat_dir+"flat.fits")
 
@@ -83,6 +79,7 @@ def reduce_orion():
     reduce_fli.clean_images(scan_img_files, out_dir, rebin=1, sky_frame=sky_dir + 'orion_sky.fits', flat_frame =flat_dir+"flat.fits")
 
     # Closed Loop - B2
+
     img_files = [data_dir + 'obj{0:03d}LS4WFS_B2_c.fits'.format(ii) for ii in fnum_c_B2]
     reduce_STA.treat_overscan(img_files)
     scan_img_files = [data_dir + 'obj{0:03d}LS4WFS_B2_c_scan.fits'.format(ii) for ii in fnum_c_B2]
@@ -106,20 +103,20 @@ def find_stars_orion():
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
     
     #Closed Loop - 4W
-    img_files = [out_dir + 'obj{0:03dLS4WFS_scan_clean.fits'.format(ii) for ii in fnum_c_4W]
-    reduce_fli.find_stars(img_files, fwhm=7, threshold=10, N_passes=2, plot_psf_compare=False, \
+    img_files = [out_dir + 'obj{0:03d}LS4WFS_c_scan_clean.fits'.format(ii) for ii in fnum_c_4W]
+    reduce_fli.find_stars(img_files, fwhm=7, threshold=15, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
 
     #Closed Loop - B2
     img_files = [out_dir + 'obj{0:03d}LS4WFS_B2_c_scan_clean.fits'.format(ii) for ii in fnum_c_B2]
-    reduce_fli.find_stars(img_files, fwhm=7, threshold=10, N_passes=2, plot_psf_compare=False, \
+    reduce_fli.find_stars(img_files, fwhm=7, threshold=15, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
 
     #Closed Loop - zc
     img_files = [out_dir + 'obj{0:03d}LS4WFS_zc21_c_scan_clean.fits'.format(ii) for ii in fnum_c_zc]
-    reduce_fli.find_stars(img_files, fwhm=7, threshold=10, N_passes=2, plot_psf_compare=False, \
+    reduce_fli.find_stars(img_files, fwhm=7, threshold=15, N_passes=2, plot_psf_compare=False, \
                               mask_flat=flat_dir+"flat.fits", mask_min=0.8, mask_max=1.4, \
                               left_slice =20, right_slice=20, top_slice=25, bottom_slice=25)
                           
@@ -135,21 +132,21 @@ def calc_star_stats():
     reduce_STA.calc_star_stats(img_files, output_stats=stats_file)
     moffat.fit_moffat(img_files, stats_file)
 
+    #Closed Loop - 4W
+    img_files = [out_dir + 'obj{0:03d}LS4WFS_c_scan_clean.fits'.format(ii) for ii in fnum_c_4W]
+    stats_file = stats_dir + 'stats_closed_4W.fits'
+    reduce_STA.calc_star_stats(img_files, output_stats=stats_file)
+    moffat.fit_moffat(img_files, stats_file)
+
     #Closed Loop - B2
-    img_files = [out_dir + 'obj{0:03d}LS_B2_c_scan_clean.fits'.format(ii) for ii in fnum_c_B2]
+    img_files = [out_dir + 'obj{0:03d}LS4WFS_B2_c_scan_clean.fits'.format(ii) for ii in fnum_c_B2]
     stats_file = stats_dir + 'stats_closed_B2.fits'
     reduce_STA.calc_star_stats(img_files, output_stats=stats_file)
     moffat.fit_moffat(img_files, stats_file)
 
-    #Closed Loop - n7
-    img_files = [out_dir + 'obj{0:03d}LSnrej7_zc21_c_scan_clean.fits'.format(ii) for ii in fnum_c_n7]
-    stats_file = stats_dir + 'stats_closed_n7.fits'
-    reduce_STA.calc_star_stats(img_files, output_stats=stats_file)
-    moffat.fit_moffat(img_files, stats_file)
-
-    #Closed Loop - n1
-    img_files = [out_dir + 'obj{0:03d}LSnrej1_zc21_c_scan_clean.fits'.format(ii) for ii in fnum_c_n1]
-    stats_file = stats_dir + 'stats_closed_n1.fits'
+    #Closed Loop - zc
+    img_files = [out_dir + 'obj{0:03d}LS4WFS_zc21_c_scan_clean.fits'.format(ii) for ii in fnum_c_zc]
+    stats_file = stats_dir + 'stats_closed_zc.fits'
     reduce_STA.calc_star_stats(img_files, output_stats=stats_file)
     moffat.fit_moffat(img_files, stats_file)
 
@@ -157,8 +154,8 @@ def calc_star_stats():
 
 
 def append_massdimm():
-
-    massdimm.fetch_data('20181221', massdimm_dir)
+    util.mkdir(massdimm_dir)
+    massdimm.fetch_data('20181222', massdimm_dir)
     stats_tables = glob.glob(root_dir + 'reduce/stats/stats*.fits')
 
     for stats in stats_tables:
@@ -179,23 +176,24 @@ def stack_orion():
     open_images = [out_dir + 'obj{0:03d}_o_scan_clean.fits'.format(ii) for ii in fnum_o]
     open_starlists = [out_dir + 'obj{0:03d}_o_scan_clean_stars.txt'.format(ii) for ii in fnum_o]
     open_output_root = stacks_dir + 'orion_stack_open'
-    reduce_fli.shift_and_add(open_images, open_starlists, open_output_root, method='mean')
+    #reduce_fli.shift_and_add(open_images, open_starlists, open_output_root, method='mean')
     
-    # Closed Loop - LS
-    closed_images = [out_dir + 'obj{0:03d}LS_B2_c_scan_clean.fits'.format(ii) for ii in fnum_c_B2]
-    closed_starlists = [out_dir + 'obj{0:03d}LS_B2_c_scan_clean_stars.txt'.format(ii) for ii in fnum_c_B2]
-    closed_output_root = stacks_dir + 'orion_stack_closed_B2'
+    # Closed Loop - 4W
+    closed_images = [out_dir + 'obj{0:03d}LS4WFS_c_scan_clean.fits'.format(ii) for ii in fnum_c_4W]
+    closed_starlists = [out_dir + 'obj{0:03d}LS4WFS_c_scan_clean_stars.txt'.format(ii) for ii in fnum_c_4W]
+    closed_output_root = stacks_dir + 'orion_stack_closed_4W'
     reduce_fli.shift_and_add(closed_images, closed_starlists, closed_output_root, method='mean')
 
     # Closed Loop - B2
-    closed_images = [out_dir + 'obj{0:03d}LSnrej7_zc21_c_scan_clean.fits'.format(ii) for ii in fnum_c_n7]
-    closed_starlists = [out_dir + 'obj{0:03d}LSnrej7_zc21_c_scan_clean_stars.txt'.format(ii) for ii in fnum_c_n7]
-    closed_output_root = stacks_dir + 'orion_stack_closed_n7'
+    closed_images = [out_dir + 'obj{0:03d}LS4WFS_B2_c_scan_clean.fits'.format(ii) for ii in fnum_c_B2]
+    closed_starlists = [out_dir + 'obj{0:03d}LS4WFS_B2_c_scan_clean_stars.txt'.format(ii) for ii in fnum_c_B2]
+    closed_output_root = stacks_dir + 'orion_stack_closed_B2'
     reduce_fli.shift_and_add(closed_images, closed_starlists, closed_output_root, method='mean')
 
-    closed_images = [out_dir + 'obj{0:03d}LSnrej1_zc21_c_scan_clean.fits'.format(ii) for ii in fnum_c_n1]
-    closed_starlists = [out_dir + 'obj{0:03d}LSnrej1_zc21_c_scan_clean_stars.txt'.format(ii) for ii in fnum_c_n1]
-    closed_output_root = stacks_dir + 'orion_stack_closed_n1'
+    # Closed Loop - zc
+    closed_images = [out_dir + 'obj{0:03dLS4WFS_zc21_c_scan_clean.fits'.format(ii) for ii in fnum_c_zc]
+    closed_starlists = [out_dir + 'obj{0:03d}LS4WFS_zc21_c_scan_clean_stars.txt'.format(ii) for ii in fnum_c_zc]
+    closed_output_root = stacks_dir + 'orion_stack_closed_zc'
     reduce_fli.shift_and_add(closed_images, closed_starlists, closed_output_root, method='mean')
     
     return
@@ -206,8 +204,8 @@ def analyze_stacks():
     open_img_files = [stacks_dir + 'orion_stack_open.fits']
 
     closed_img_files = [stacks_dir + 'orion_stack_closed_B2.fits', \
-                        stacks_dir + 'orion_stack_closed_n7.fits', \
-                        stacks_dir + 'orion_stack_closed_n1.fits']
+                        stacks_dir + 'orion_stack_closed_4W.fits', \
+                        stacks_dir + 'orion_stack_closed_zc.fits']
     
     #Find stars in image
     reduce_fli.find_stars(open_img_files, fwhm=10, threshold=10, N_passes=2, plot_psf_compare=False, \
